8d5f85217129c4cc4aeff8649874ce6f570b75a5
jiopwls <72438639+jiopwls@users.noreply.github.com>
2020-10-26 20:42:24 +0900

    Add files via upload

--- js/sub2_2.js
+++ js/sub2_2.js
@@ -3,7 +3,7 @@ $(function(){
       var s = $(window).scrollTop();  
       console.log(s);
 
-      if (s >= 350) {
+      if (s >= 200) {
         $('#menu > .menu_nav').addClass('show');
         $('#menu > .menu_nav').removeClass('hide');
       } else {
