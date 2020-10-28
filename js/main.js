6aa06e091104dc50dcca25a2b57e6fe246214277
jiopwls <72438639+jiopwls@users.noreply.github.com>
2020-10-28 17:58:56 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -87,4 +87,20 @@ $(function () {
     changeSlider();
   })
 
-}) // $end
+});
+$(function(){
+  $(document).scroll(function(){
+      var s = $(window).scrollTop();  
+      console.log(s);
+
+      if (s >= 1200) {
+        $('#menu > .menu_nav').addClass('show');
+        $('#menu > .menu_nav').removeClass('hide');
+      } else {
+        $('#menu > .menu_nav').removeClass('show');
+        $('#menu > .menu_nav').addClass('hide');
+
+      }
+  })
+});    
+ // $end
